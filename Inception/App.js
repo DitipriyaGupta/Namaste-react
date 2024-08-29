fe6bb2fbbf5aa645d9621e89463ebd7a02f40ad2
DitipriyaGupta <mail.ditipriyagupta@gmail.com>
2024-08-29 15:31:18 +0530

    first commit

---
+++ Inception/App.js
@@ -0,0 +1,38 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Hello World"
+);
+console.log(heading);
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+
+// Nested parent-child
+
+// Reference
+{
+  /* <div id="parent">
+<div id="child">
+    <h1>This is a h1 tag</h1>
+</div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    // Siblings/Array of children
+    [
+      React.createElement("h1", {}, "This is a h1 tag")      ,
+      React.createElement("h2", {}, "This is a h2 tag"),
+    ]
+  )
+);     
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
